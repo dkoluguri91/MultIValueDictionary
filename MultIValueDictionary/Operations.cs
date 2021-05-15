dc6eb588e4be6f42c3d80d7b65380b0669ecce65
koluguri dinesh <kolugd@gmail.com>
2021-05-15 17:33:45 -0500

    Initial commit

---
+++ MultIValueDictionary/Operations.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultIValueDictionary
+{
+    class Operations
+    {
+
+        public bool FindValue(IDictionary<string, List<string>> Values, string[] inputKey)
+        {
+            foreach (var item in Values)
+            {
+                if (item.Key == inputKey[0] && item.Value.Contains(inputKey[1]))
+                    return true;
+            }
+
+            return false;
+        }
+        public IDictionary<string, List<string>> AddValue(IDictionary<string, List<string>> Values, string[] input)
+        {
+            if (FindValue(Values,input))
+            {
+                Console.WriteLine("Error, value already exists.");
+            }
+            else if(Values.ContainsKey(input[0]))
+            {
+                foreach (var val in Values)
+                {
+                    if (val.Key == input[0])
+                    {
+                        val.Value.Add(input[1]);
+                    }
+                }
+                Console.WriteLine("Added");
+            }
+            else
+            {
+                Values.Add(new KeyValuePair<string,List<string>>(input[0], new List<string>() { input[1] }));
+                Console.WriteLine("Added");
+            }
+            return Values;
+        }
+        public void GetAllKeys(IDictionary<string, List<string>> Values)
+        {
+            if (Values.Count == 0)
+            {
+                Console.WriteLine("Multi Value Dictionary is empty.");
+            }
+            else
+            {
+                foreach (var value in Values)
+                {
+                    Console.WriteLine(value.Key);
+                }
+            }
+        }
+        public void GetMembersWithKey(IDictionary<string, List<string>> Values, string inputKey)
+        {
+            if (Values.ContainsKey(inputKey))
+            {
+                foreach (var item in Values)
+                {
+                    if (item.Key == inputKey)
+                    {
+                        foreach (var itemVal in item.Value)
+                        {
+                            Console.WriteLine(itemVal);
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error, Key does not exit.");
+            }
+        }
+        public IDictionary<string, List<string>> Remove(IDictionary<string, List<string>> Values, string[] inputKey)
+        {
+            if (Values.ContainsKey(inputKey[0]) &&FindValue(Values, inputKey))
+            {
+                foreach (var item in Values)
+                {
+                    if (item.Key == inputKey[0] && item.Value.Count > 1)
+                    {
+                        foreach (var itemval in item.Value)
+                        {
+                            if (itemval == inputKey[1])
+                            {
+                                item.Value.Remove(inputKey[1]);
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("Removed");
+                    }
+                    else if (item.Key == inputKey[0] && item.Value.Count == 1)
+                    {
+                        Values.Remove(item);
+                        Console.WriteLine("Removed");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error, Value does not exit.");
+            }
+            return Values;
+        }
+        public IDictionary<string, List<string>> RemoveAll(IDictionary<string, List<string>> Values, string inputKey)
+        {
+            if (Values.ContainsKey(inputKey))
+            {
+                Values.Remove(inputKey);
+                Console.WriteLine("Removed");
+            }
+            else
+            {
+                Console.WriteLine("Error, Key does not exit.");
+            }
+            return Values;
+        }
+        public IDictionary<string, List<string>> Clear(IDictionary<string, List<string>> Values)
+        {
+            return new Dictionary<string, List<string>>();
+        }
+        public void AllMembers(IDictionary<string, List<string>> Values)
+        {
+            if (Values.Count > 0)
+            {
+                foreach (var val in Values)
+                {
+                    foreach (var item in val.Value)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Empty,set");
+            }
+        }
+        public void Items(IDictionary<string, List<string>> Values)
+        {
+            if (Values.Count > 0)
+            {
+                foreach (var val in Values)
+                {
+                    foreach (var item in val.Value)
+                    {
+                        Console.WriteLine(val.Key + ":" + item);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Empty,set");
+            }
+        }
+    }
+}
---
+++ MultIValueDictionary/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultIValueDictionary
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Multi Value Dictionary. ");
+            IDictionary<string, List<string>> MultiValue = new Dictionary<string, List<string>>();
+            Operations operations = new Operations();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("**************************************************************");
+                Console.WriteLine("Choose the option for Multi Value Dictionary Operation : ");
+                Console.WriteLine("options  - Operation ");
+                Console.WriteLine(" 1           ADD");
+                Console.WriteLine(" 2           KEYS");
+                Console.WriteLine(" 3           MEMBERS");
+                Console.WriteLine(" 4           REMOVE");
+                Console.WriteLine(" 5           REMOVEALL");
+                Console.WriteLine(" 6           CLEAR");
+                Console.WriteLine(" 7           KEYEXISTS");
+                Console.WriteLine(" 8           VALUEEXISTS");
+                Console.WriteLine(" 9           ALLMEMBERS");
+                Console.WriteLine(" 10          ITEMS");
+                Console.WriteLine("**************************************************************");
+                Console.Write("Please enter option :");
+                option = Convert.ToInt32(Console.ReadLine());
+                string input = "";
+                string[] inputKeyValue = null;
+                switch (option)
+                {
+
+                    case 1:
+                        // code block
+                        Console.Write("Please enter key value with space separation :");
+                        inputKeyValue = Console.ReadLine().Split(" ");
+                        MultiValue = operations.AddValue(MultiValue, inputKeyValue);
+                        break;
+                    case 2:
+                        // code block
+                        operations.GetAllKeys(MultiValue);
+                        break;
+                    case 3:
+                        // code block
+                        Console.Write("please enter the key :");
+                        input = Console.ReadLine();
+                        operations.GetMembersWithKey(MultiValue, input);
+                        break;
+                    case 4:
+                        // code block
+                        Console.Write("Please enter the key , value with space separation :");
+                        inputKeyValue = Console.ReadLine().Split(" ");
+                        MultiValue = operations.Remove(MultiValue, inputKeyValue);
+                        break;
+                    case 5:
+                        // code block
+                        Console.Write("Please enter the key :");
+                        input = Console.ReadLine();
+                        MultiValue = operations.RemoveAll(MultiValue, input);
+                        break;
+                    case 6:
+                        // code block
+                        MultiValue = operations.Clear(MultiValue);
+                        Console.WriteLine("Cleared");
+                        break;
+                    case 7:
+                        // code block
+                        Console.Write("Please enter the key :");
+                        input = Console.ReadLine();
+                        Console.WriteLine(MultiValue.ContainsKey(input));
+                        break;
+                    case 8:
+                        // code block
+                        Console.Write("Please enter the key , value with space separation :");
+                        inputKeyValue = Console.ReadLine().Split(" ");
+                        Console.WriteLine(operations.FindValue(MultiValue, inputKeyValue));
+                        break;
+                    case 9:
+                        // code block
+                        operations.AllMembers(MultiValue);
+                        break;
+                    case 10:
+                        // code block
+                        operations.Items(MultiValue);
+                        break;
+                    default:
+                        // code block
+                        Console.WriteLine("Please choice right option.");
+                        break;
+                }
+                Console.WriteLine("**************************************************************");
+                Console.WriteLine("Please Press 1 to continue , Press 0 to Exit. ");
+                option = Convert.ToInt32(Console.ReadLine());
+            } while (option != 0);
+
+
+        }
+    }
+}
